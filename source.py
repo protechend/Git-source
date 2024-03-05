3ff3a541ce28426fc6c743e38a6174e244c9a5c3
protechend <shashank.chopra.maac@gmail.com>
2024-03-05 13:53:11 +0530

    First Commit

---
+++ source.py
@@ -0,0 +1,10 @@
+# Define a function to add two numbers
+def add_numbers(number1, number2):
+    return number1 + number2
+
+# Call the function with sample inputs
+result = add_numbers(5, 7)
+
+# Print the result
+print("The sum is:", result)
+
